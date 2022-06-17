866788e3ae87a385d824720ec9dcdbff22f1e71f
graample <haesik.yee@gmail.com>
2022-06-17 14:30:40 -0700

    swaps from MUI cards to accordions

--- client/src/components/BreweryListItem.jsx
+++ client/src/components/BreweryListItem.jsx
@@ -1,47 +1,40 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import GoogleMap from './GoogleMap.jsx';
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
-
 export default function BreweryListItem(props) {
   var center;
-  {props.item.latitude
+  props.item.latitude
     ? center = [parseFloat(props.item.latitude), parseFloat(props.item.longitude)]
-    : center = null}
+    : center = null
   return (
     <>
-      <Card sx={{ maxWidth: 300 }}>
-        <CardContent>
-          <Typography variant="h5" component="div">
-            {props.item.name}
-          </Typography>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>{props.item.name}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
           <Typography sx={{ mb: 1.5 }} color="text.secondary">
             {props.item.street ? props.item.street : ''} {props.item.city}, {props.item.state}, {props.item.postal_code}
           </Typography>
-        </CardContent>
-        <CardActions>
           {props.item.website_url
           ? <Button size="small" href={props.item.website_url} target="_blank">Website</Button>
           : <Typography sx={{ mb: 1.5 }} color="text.secondary">
           No website provided
           </Typography>}
-        </CardActions>
-        {center ? <GoogleMap center={center}/> : ''}
-      </Card>
+          {center ? <GoogleMap center={center}/> : ''}
+        </AccordionDetails>
+      </Accordion>
     </>
   )
 }
