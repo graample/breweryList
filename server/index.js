721b66014364263c3c5e18f6f43c4d75334ab13f
Raymond Yee <haesik.yee@gmail.com>
2022-06-19 13:24:59 -0700

    change 7

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const db = require('./db/postgres.js');
+// const db = require('./db/postgres.js');
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
